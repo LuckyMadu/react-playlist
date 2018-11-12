0db06cc5e497b4728c1c2b5bae62d14ede0860ca
unknown <you@example.com>
2018-11-12 23:00:30 +0530

    display only ages above 25

--- myapp/src/Lahiru.js
+++ myapp/src/Lahiru.js
@@ -1,16 +1,41 @@
 import React from "react";
 
-const Lahiru = ({lahiru}) => {
+const Lahiru = ({ lahiru }) => {
   //const { lahiru } = props};
-  const lahiruList = lahiru.map(lahiru => {
-    return (
+  /* const lahiruList = lahiru.map(lahiru => {
+    if (lahiru.age > 30) {
+      return (
+        <div className="lahiru" key={lahiru.id}>
+          <div>Name: {lahiru.name}</div>
+          <div>Age: {lahiru.age}</div>
+        </div>
+      );
+    } else {
+      return null; 
+    }
+  }); */
+  /* const lahiruList = lahiru.map(lahiru => {
+    return lahiru.age > 20 ? (
       <div className="lahiru" key={lahiru.id}>
         <div>Name: {lahiru.name}</div>
         <div>Age: {lahiru.age}</div>
       </div>
-    );
-  });
-  return <div className="lahiru-list">{lahiruList}</div>;
+    ) : null;
+  }); */
+  return (
+    <div className="lahiru-list">
+      {/* {lahiruList} */}
+
+      {lahiru.map(lahiru => {
+        return lahiru.age > 20 ? (
+          <div className="lahiru" key={lahiru.id}>
+            <div>Name: {lahiru.name}</div>
+            <div>Age: {lahiru.age}</div>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Lahiru;
