593edc198ca43027f5a872f5e18906d38ffb8b10
unknown <you@example.com>
2018-11-12 18:57:01 +0530

    passing object and using map method

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from "react";
 import Lahiru from "./Lahiru";
 
 class App extends Component {
+  state = {
+    lahiru: [
+      {
+        name: "Lahiru",
+        age: 20,
+        id:1
+      },
+      {
+        name: "Madu",
+        age: 45,
+        id:2
+      },
+      {
+        name: "Amara",
+        age: 30,
+        id:3
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>My react App</h1>
-        <Lahiru name="Madu" age="40" />
-        <Lahiru name="Amara" age="25" />
+        {/* <Lahiru name="Madu" age="40" /> */}
+        <Lahiru lahiru={this.state.lahiru} />
       </div>
     );
   }
--- myapp/src/Lahiru.js
+++ myapp/src/Lahiru.js
@@ -3,15 +3,19 @@ import React, { Component } from "react";
 class Lahiru extends Component {
   render() {
     //console.log(this.props);
-    const { name, age } = this.props;
-    return (
-      <div className="lahiru">
-        {/* <div>Name: {this.props.name}</div>
+    //const { name, age } = this.props;
+    const { lahiru } = this.props;
+    const lahiruList = lahiru.map(lahiru => {
+      return (
+        <div className="lahiru" key={lahiru.id}>
+          {/* <div>Name: {this.props.name}</div>
         <div>Age: {this.props.age}</div> */}
-        <div>Name: {name}</div>
-        <div>Age: {age}</div>
-      </div>
-    );
+          <div>Name: {lahiru.name}</div>
+          <div>Age: {lahiru.age}</div>
+        </div>
+      );
+    });
+    return <div className="lahiru-list">{lahiruList}</div>;
   }
 }
 
