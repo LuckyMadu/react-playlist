abd74b368a16b88e5459a55ae5c613eb58eee5d4
unknown <you@example.com>
2018-11-12 10:42:26 +0530

    create a new component called Lahiru and add it to App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Lahiru from "./Lahiru";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My react App</h1>
+        <Lahiru />
       </div>
     );
   }
---
+++ myapp/src/Lahiru.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Lahiru extends Component {
+  render() {
+    return (
+      <div className="lahiru">
+        <div>Name: Lahiru</div>
+        <div>Age: 20</div>
+      </div>
+    );
+  }
+}
+
+export default Lahiru;
