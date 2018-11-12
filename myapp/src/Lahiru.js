dc60fea01df37ad3ef69bf3c2e64a9dc2ae1f6c2
unknown <you@example.com>
2018-11-12 19:49:47 +0530

    container components with state made by classes and ui components without state made by functions

--- myapp/src/Lahiru.js
+++ myapp/src/Lahiru.js
@@ -1,22 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Lahiru extends Component {
-  render() {
-    //console.log(this.props);
-    //const { name, age } = this.props;
-    const { lahiru } = this.props;
-    const lahiruList = lahiru.map(lahiru => {
-      return (
-        <div className="lahiru" key={lahiru.id}>
-          {/* <div>Name: {this.props.name}</div>
-        <div>Age: {this.props.age}</div> */}
-          <div>Name: {lahiru.name}</div>
-          <div>Age: {lahiru.age}</div>
-        </div>
-      );
-    });
-    return <div className="lahiru-list">{lahiruList}</div>;
-  }
-}
+const Lahiru = ({lahiru}) => {
+  //const { lahiru } = props};
+  const lahiruList = lahiru.map(lahiru => {
+    return (
+      <div className="lahiru" key={lahiru.id}>
+        <div>Name: {lahiru.name}</div>
+        <div>Age: {lahiru.age}</div>
+      </div>
+    );
+  });
+  return <div className="lahiru-list">{lahiruList}</div>;
+};
 
 export default Lahiru;
